f80407b4e43ec1bb0499309ff107d5d5f1c0b24d
Masen Matthews <masenmatthews@gmail.com>
2018-11-30 22:13:42 -0800

    add initial content to vision page, add module for collapsables

--- src/pages/vision.js
+++ src/pages/vision.js
@@ -5,6 +5,18 @@ import Layout from "../components/layout"
 export default ({ data }) => (
   <Layout>
     <h1>Vision</h1>
+    <h3>Helpful shortcuts</h3>
+    <p>Zoom in</p>
+      <ul>CMND + (Mac)</ul>
+      <ul>CTRL + (Windows)</ul>
+    <p>Zoom out</p>
+      <ul>CMND - (Mac)</ul>
+      <ul>CTRL - (Windows)</ul>
+    <h3>Making navigation easier for people with vision problems</h3>
+    <h4>Always include the following:</h4>
+      <ol>1. Concise page titles (i.e. "Vision - AccessiReference")</ol>
+      <ol>2. Primary language of the page within the HTML element (i.e. html lang="en")</ol>
+      <ol>*These help screen readers communicate information in an effective way!</ol>
   </Layout>
 )
 
