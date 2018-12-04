f347d58e6ab95153dd34767c187a549dabd1ee7d
Masen Matthews <masenmatthews@gmail.com>
2018-12-04 13:00:31 -0800

    add comments denoting styling bug in vision.js, add initial content to about.js

--- src/pages/vision.js
+++ src/pages/vision.js
@@ -1,17 +1,21 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Collapsible from 'react-collapsible';
 
 export default ({ data }) => (
   <Layout>
     <h1>Vision</h1>
-    <h3>Helpful shortcuts</h3>
+    {/* Need to figure out how to style collapsible trigger - CSS module has been tricky */}
+    <Collapsible
+      trigger="Helpful shortcuts (click to expand)">
     <p>Zoom in</p>
       <ul>CMND + (Mac)</ul>
       <ul>CTRL + (Windows)</ul>
     <p>Zoom out</p>
       <ul>CMND - (Mac)</ul>
       <ul>CTRL - (Windows)</ul>
+    </Collapsible>
     <h3>Making navigation easier for people with vision problems</h3>
     <h4>Always include the following:</h4>
       <ol>1. Concise page titles (i.e. "Vision - AccessiReference")</ol>
