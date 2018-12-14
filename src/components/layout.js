c1a8df258646f72874ea4fc876291bbfc99c8908
Masen Matthews <masenmatthews@gmail.com>
2018-12-14 11:50:45 -0800

    refactor, delete header module and unneccesary code in layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,10 @@ import { css } from "react-emotion"
 import { StaticQuery, Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Header from "../components/header"
-import headerStyles from "./header.module.css"
 import "./layout.css"
 
 export default ({ children }) => (
   <Header>
-    <div className={headerStyles.header}>{children}</div>
+    <div>{children}</div>
   </Header>
 )
